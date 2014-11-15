9fa2764c71184ee7deb8150b184d61a182761223
David Ochoa <radikal_david@hotmail.com>
2014-11-15 02:55:57 -0600

    15-Nov-3
    Arreglados errores en la seccion de horarios

--- schedule.php
+++ schedule.php
@@ -1,52 +1,69 @@
 <html>
+
 <head>
-	<title>TeacherOnLine</title>
-	<meta charset="utf-8">
-		
-		<?php
+<title>TeacherOnLine</title>
+<meta charset="utf-8">
+<?php
 		include('HeaderBar.html');
-	?>
-	
-		<!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css"> -->
+		?>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css">
+
 		<style>
-		.feedback { font-size: 1.4em; }
-		.selectable .ui-selecting { background: #2ECCFA; }
-		.selectable .ui-selected { background: #04B431; color: white; }
-		.selectable { list-style-type: none; margin: 0; padding: 0; width: 1%; }
-		.selectable li { margin: 0px; font-size: 1.4em; width: 5em; height: 2em;}
+				.feedback {
+					font-size: 1.4em;
+				}
+				.selectable .ui-selecting {
+					background: #2ECCFA;
+				}
+				.selectable .ui-selected {
+					background: #04B431;
+					color: white;
+				}
+				.selectable {
+					list-style-type: none;
+					margin: 0;
+					padding: 0;
+					width: 1%;
+				}
+				.selectable li {
+					margin: 0px;
+					font-size: 1.4em;
+					width: 5em;
+					height: 2em;
+				}
 		</style>
-		
-		<script type="javascript">
-		$(function() {
-		$( ".selectable" ).selectable();
-		});
+
+		<script>
+			$(function() {
+			$( ".selectable" ).selectable();
+			});
 		</script>
-		
-		<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
 
-	<div>
-
-	<table border="0">
-			<tr align="right">
-				<td align="right" colspan="8"><button class="btn" onclick="updateSchedule()">Save</button></td>
-			</tr>
-			<tr class="navbar">
-				<th></th>
-				<th>Lunes</th>
-				<th>Martes</th>
-				<th>Mi&eacute;rcoles</th>
-				<th>Jueves</th>
-				<th>Viernes</th>
-				<th>S&aacute;bado</th>
-				<th>Domingo</th>
-			</tr>
-			<tr>
-				<td>
-					7:30
-				</td>
-				<?php 
+<div>
+	<table border="0" align="center">
+		<tr align="right">
+			<td align="right" colspan="8">
+			<button class="btn" onclick="updateSchedule()">Save</button></td>
+		</tr>
+		<tr class="navbar">
+			<th class="text-center h5">Lunes</th>
+			<th class="text-center h5">Martes</th>
+			<th class="text-center h5">Miércoles</th>
+			<th class="text-center h5">Jueves</th>
+			<th class="text-center h5">Viernes</th>
+			<th class="text-center h5">Sábado</th>
+			<th class="text-center h5">Domingo</th>
+		</tr>
+		<tr>
+			<td>7:30 </td>
+			<?php 
 					for ($j=0; $j < 7; $j++) { 
 						echo "<td rowspan='27'>
 							<ol class='selectable'>";
@@ -56,32 +73,28 @@
 						echo "<ol>
 							</td>";	
 					}
-
 				?>
-			</tr>
-			<?php 
+		</tr>
+		<?php 
 				for ($i=8; $i < 21; $i++) { 
 					echo "<tr>
-						<td>
-							".$i.":00
+					<td>
+						".$i.":00
 						</td>
 					</tr>";	
 					echo "<tr>
-						<td>
-							".$i.":30
+					<td>
+						".$i.":30
 						</td>
 					</tr>";
 				}
-				
 			?>
-			<tr align="right">
-				<td align="right" colspan="8"><button class="btn">Save</button></td>
-			</tr>
+		<tr align="right">
+			<td align="right" colspan="8"><button class="btn">Save</button></td>
+		</tr>
 	</table>
+</div>
 
-	</div>
-
-
-	
 </body>
+
 </html>
